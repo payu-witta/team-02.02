9ed59b471d01b2ced364b573c893b1f8bb31f3fc
Maithili Revankar <maithilirevankar@1x-nat-vl932-172-30-205-143.wireless.umass.edu>
2026-04-28 10:48:01 -0400

    refactor: improve readability of RSVP sorting logic

--- src/rsvp/MyRsvpsService.ts
+++ src/rsvp/MyRsvpsService.ts
@@ -49,8 +49,13 @@ class MyRsvpsService implements IMyRsvpsService {
         history.push(item);
       }
     }
-    upcoming.sort((a,b) => a.event.startDatetime.getTime() - b.event.startDatetime.getTime());
-    history.sort((a,b) => b.event.startDatetime.getTime() - a.event.startDatetime.getTime());
+    upcoming.sort(
+      (a,b) => a.event.startDatetime.getTime() - b.event.startDatetime.getTime()
+    );
+    history.sort(
+      (a,b) => b.event.startDatetime.getTime() - a.event.startDatetime.getTime()
+    );
+    
     return Ok({ upcoming, history });
   }
 }
